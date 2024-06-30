3c9be3bfcf9083910891cd33789bac71554a4e24
ayodhya2004 <ashiniayodhya3@gmail.com>
2024-06-30 19:56:36 +0530

    Add files via upload

---
+++ TreeNode2.java
@@ -0,0 +1,15 @@
+package binarytree;
+
+public class TreeNode2 {
+	    public static int height = 0;
+		int val;
+	    TreeNode2 left;
+	    TreeNode2 right;
+
+
+	    public TreeNode2(int val) {
+	        this.val = val;
+	        this.left = null;
+	        this.right = null;
+	    }
+	}
